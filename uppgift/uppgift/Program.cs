45f1a478ed13d249686e1ec50b5c8c61b0a0bf04
Admin <Admin@TEBELEVDISK>
2019-10-23 14:56:08 +0200

    har skrivit att taler bytes på två variable

---
+++ uppgift/uppgift/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 10;
+            BytPlats(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+        }
+
+        public static void BytPlats(ref int a, ref int b)
+        {
+            int byt = a;
+            a = b;
+            b = byt;
+
+        }
+    }
+}
